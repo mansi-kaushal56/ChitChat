6b75aeccd65d3604d87415cb6a048216e38b4427
Mansi Kaushal <kaushal.mansi.tdv@gmail.com>
2024-11-26 11:53:44 +0530

    first commit

---
+++ ChitChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChitChatApp
+//
+//  Created by Raj Mohan on 26/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
